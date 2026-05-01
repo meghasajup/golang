a4a8e8f1f951fb0dfa21aedcd967a56692e399c4
meghasajup <meghasaju45@gmail.com>
2026-05-01 19:57:25 +0530

    Variables and types

--- day-1/main.go
+++ day-1/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Start building. No excuses")
+
+	//Variable + type
+	var name string = "Megha"
+	age := 24
+	isDev := true
+	fmt.Printf("My name is %v and my age is %v. And this is %v\n", name, age, isDev)
+
+	//Type
+	fmt.Printf("%T\n", name)
+	fmt.Printf("%T\n", age)
+	fmt.Printf("%T\n", isDev)
+
 }
